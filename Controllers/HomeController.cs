b9dd8bb770a652676b852b964deb7fc010f1b3b0
wtf2408 <bogdanevseyv@yandex.ru>
2025-03-03 19:55:44 +0300

    fixed some problems

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,15 +7,17 @@ namespace YogaMasterskyaMVC.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(_configuration);
         }
     }
 }
